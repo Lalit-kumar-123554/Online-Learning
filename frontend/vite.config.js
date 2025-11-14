c41b173aeabdc84b83ab111362eede5aedd88db5
Lalit-kumar-123554 <sreelalitkumar@gmail.com>
2025-11-14 12:31:47 +0530

    Fixed Vite base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/Online-Learning/',  // 👈 add this line
+  base: "./",   // 🔥 THIS FIXES YOUR BLANK PAGE
   plugins: [react()],
-})
+});
